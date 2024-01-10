2df9caf014ceb551d9881a51028caa9edd220095
anacmezes <anamenezes.dev@gmail.com>
2024-01-10 10:54:43 -0300

    Repository and service configured

---
+++ src/main/java/com/delume/repositories/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.delume.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.delume.domain.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ src/main/java/com/delume/services/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.delume.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+}
