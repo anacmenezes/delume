5edc2b31b0a02fc2657e4c6b54dc1a5c0b81a0c1
anacmezes <anamenezes.dev@gmail.com>
2024-01-10 10:10:30 -0300

    Endpoints configured for swagger

--- src/main/java/com/delume/DelumeApplication.java
+++ src/main/java/com/delume/DelumeApplication.java
@@ -1,13 +1,22 @@
 package com.delume;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
-public class DelumeApplication {
+@OpenAPIDefinition(info = @Info(title = "Find Profile", version = "1", description = "Integration of instagram profiles in google search"))
+public class DelumeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DelumeApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... enderecoeRepository) throws Exception {
 	}
 
 }
